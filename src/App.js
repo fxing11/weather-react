2d1feaa6bf24bb6a1fa84a52c04526f48e769cb5
Fei Xing <56478202+fxing11@users.noreply.github.com>
2022-01-31 15:54:36 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import Weather from "./Weather";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Fei Xing and is{" "}
+        <a
+          href="https://github.com/fxing11/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced
+        </a>{" "}
+        on GitHub
+      </footer>
     </div>
   );
 }
-
-export default App;
