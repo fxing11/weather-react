f375972b2cb41fe08b9d0293b8c6497725e03691
Fei Xing <56478202+fxing11@users.noreply.github.com>
2022-02-02 13:20:37 +0100

    fixed css issue

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,10 +16,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <div className="float-left">
+            <div>
               <WeatherIcon code={props.data.icon} />
             </div>
-            <div className="float-left">
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./WeatherTemperature.css";
+import "./Weather.css";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
