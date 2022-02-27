5c6287dc0ce7eb549bdcc0bb3c81e737b30f1b9a
Fei Xing <56478202+fxing11@users.noreply.github.com>
2022-02-27 17:30:53 +0100

    added api for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import Axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handelResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+
+  let apiKey = "48477574ab33f03aeefc4b00219c6195";
+  let longtitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
+
+  Axios.get(apiUrl).then(handelResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
